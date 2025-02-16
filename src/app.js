78f926cd2c86cac3b89119d1ae8a136fad3beada
Ankit8412226 <125825310+Ankit8412226@users.noreply.github.com>
2025-02-16 16:56:48 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -3,10 +3,9 @@ const http = require("http");
 const cors = require("cors"); // Import cors
 const Routes = require("./routes/index");
 const dbConnect = require("./config/dbConnect");
-const socketIO = require("socket.io");
+
 const path = require("path");
 
-// Initialize Express app and create HTTP server
 const app = express();
 const server = http.createServer(app);
 
@@ -15,14 +14,14 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Connect to MongoDB
 dbConnect();
 
-// Connect to MongoDB
-dbConnect();
-
 // Middleware
 const corsOptions = {
-  origin: "*",
+  origin: "*", // Allows all origins (both HTTP and HTTPS)
   methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // Allow credentials if needed
 };
+
 app.use(express.json());
 app.use(cors(corsOptions));
 
